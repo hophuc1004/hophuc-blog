e3e50c4804ab055cf6efeb5b9f953010721d09b8
1004hophuc <hophuc1004@gmail.com>
2025-07-26 10:34:50 +0700

    create source hophuc blog

---
+++ data/headerNavLinks.ts
@@ -0,0 +1,9 @@
+const headerNavLinks = [
+  { href: '/', title: 'Home' },
+  { href: '/blog', title: 'My Blogs' },
+  // { href: '/tags', title: 'Tags' },
+  // { href: '/projects', title: 'Projects' },
+  { href: '/my-profile', title: 'My Profile' },
+]
+
+export default headerNavLinks
